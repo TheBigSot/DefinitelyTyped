55e13651b06fd6837fb1f20f97f41f8e01a39a04
TheBigSot <sot_c2@hotmail.com>
2018-01-14 22:21:59 -0600

    Update index.d.ts

--- universal-cookie/index.d.ts
+++ universal-cookie/index.d.ts
@@ -1,13 +1,13 @@
 // Type definitions for universal-cookie 2.1.2
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
- 	 
+declare module 'universal-cookie';
 
 interface Cookies { 
 	new (cookieHeader?: string | object): Cookies;
 		 
-	get(name: string, options?: CookieGetOptions): void; 
-	getAll(options?: CookieGetOptions): void; 
+	get(name: string, options?: CookieGetOptions): string; 
+	getAll(options?: CookieGetOptions): any; 
 	set(name: string, value: string, options?: CookieUpdateOptions): void;  
 	remove(name: string, options?: CookieUpdateOptions): void;  
 }
@@ -20,5 +20,5 @@ interface CookieUpdateOptions {
 	maxAge?: number, 
 	domain?: string, 
 	secure?: boolean, 
-	httpOnly?: boolean
+	httpOnly?: boolean,
 }
